58661ce0ed581f50b21c48e6584fdd7778e2049a
Artemis <Artemis25265@gmail.com>
2026-01-11 10:04:11 -0500

    get rid of error

--- lua/config/Lazy/lsp.lua
+++ lua/config/Lazy/lsp.lua
@@ -43,9 +43,6 @@ return {
 		require("mason-lspconfig").setup({
 			ensure_installed = {
 				"lua_ls",
-				"rust_analyzer",
-				"gopls",
-				"tailwindcss",
                 "vim",
                 "vimdoc"
 			},

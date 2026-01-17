fc89b5f19eef9194d78c32d2d8aed3e85ac990ec
Artemis <Artemis25265@gmail.com>
2026-01-16 20:51:23 -0500

    add formatting

--- lua/config/Lazy/lsp.lua
+++ lua/config/Lazy/lsp.lua
@@ -26,9 +26,46 @@ return {
 	},
 
 	config = function()
-        require("luasnip.loaders.from_vscode").lazy_load()
+		require("luasnip.loaders.from_vscode").lazy_load()
 		require("conform").setup({
-			formatters_by_ft = {},
+			formatters = {
+				prettier = {
+					args = {
+						"--tab-width",
+						"4",
+						"--stdin-filepath",
+						"$FILENAME",
+					},
+				},
+				stylua = {
+					args = {
+						"--tab-width",
+						"4",
+						"--stdin-filepath",
+						"$FILENAME",
+					},
+				},
+			},
+
+			formatters_by_ft = {
+				lua = { "stylua" },
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				css = { "prettier" },
+				html = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				graphql = { "prettier" },
+			},
+
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			},
 		})
 		local cmp = require("cmp")
 		local cmp_lsp = require("cmp_nvim_lsp")
@@ -67,23 +104,23 @@ return {
 					vim.g.zig_fmt_parse_errors = 0
 					vim.g.zig_fmt_autosave = 0
 				end,
-                ["lua_ls"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.lua_ls.setup {
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                format = {
-                                    enable = true,
-                                    defaultConfig = {
-                                        indent_style = "tab",
-                                        indent_size = "4",
-                                    }
-                                },
-                            }
-                        }
-                    }
-                end,
+				["lua_ls"] = function()
+					local lspconfig = require("lspconfig")
+					lspconfig.lua_ls.setup({
+						capabilities = capabilities,
+						settings = {
+							Lua = {
+								format = {
+									enable = true,
+									defaultConfig = {
+										indent_style = "tab",
+										indent_size = "4",
+									},
+								},
+							},
+						},
+					})
+				end,
 				["tailwindcss"] = function()
 					local lspconfig = require("lspconfig")
 					lspconfig.tailwindcss.setup({
